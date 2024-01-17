31b31fae51af2feb9d3c3a5609a134fd7121c6e2
Raúl Morales <raulmdaw@gmail.com>
2024-01-17 19:47:13 +0100

    152. PrimeNG - Menú superior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -13,9 +15,10 @@ import { ButtonModule } from 'primeng/button';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
 
-    ButtonModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MenuModule
+  ]
+})
+export class PrimeNgModule { }
