5e97a00004e5b2e7c9f1bd097ebd0b61f2ebf584
Raúl Morales <raulmdaw@gmail.com>
2024-01-17 20:06:13 +0100

    153. PrimeNg - MenuBar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+
 import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [],
   exports: [
-    MenuModule
+    MenubarModule,
+    MenuModule,
   ]
 })
 export class PrimeNgModule { }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -4,16 +4,42 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
-  styles: ``
+  styles: ``,
 })
 export class MenuComponent implements OnInit {
   public menuItems: MenuItem[] = [];
+  public items: MenuItem[] | undefined;
 
   ngOnInit(): void {
     this.menuItems = [
-      {label: 'New', icon: 'pi pi-fw pi-plus'},
-      {label: 'Open', icon: 'pi pi-fw pi-download'},
-      {label: 'Undo', icon: 'pi pi-fw pi-refresh'},
-    ]
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          },
+        ],
+      },
+    ];
   }
 }
