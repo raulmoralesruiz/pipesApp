011941c34717f9aa8aec8eb55de47152e59cc3ba
Raúl Morales <raulmdaw@gmail.com>
2024-01-17 20:26:09 +0100

    154. Módulos y Rutas de mi aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicsPageComponent } from './products/pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './products/pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './products/pages/uncommon-page/uncommon-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
