f3c923ad349d2fd7ee5739eaf0ce30115b9d8f99
Raúl Morales <raulmdaw@gmail.com>
2024-01-18 20:39:40 +0100

    166. AsyncPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
   // i18n Select
   public name: string = 'Raúl';
-  public gender: 'male'|'female' = 'male';
+  public gender: 'male' | 'female' = 'male';
   public invitationMap = {
     male: 'invitarlo',
-    female: 'invitarla'
-  }
+    female: 'invitarla',
+  };
 
-  changeClient():void {
+  changeClient(): void {
     if (this.name === 'Raúl') {
       this.name = 'Meli';
       this.gender = 'female';
@@ -25,13 +26,21 @@ export class UncommonPageComponent {
   }
 
   // i18n Plural
-  public clients: string[] = ['Leti','Raúl','Carlos','Meli','Manuel','Jimena','Cristina'];
+  public clients: string[] = [
+    'Leti',
+    'Raúl',
+    'Carlos',
+    'Meli',
+    'Manuel',
+    'Jimena',
+    'Cristina',
+  ];
   public clientsMap = {
     '=0': 'no tenemos ningún cliente esperando.',
     '=1': 'tenemos un cliente esperando.',
     '=2': 'tenemos 2 personas esperando.',
-    'other': 'tenemos # clientes esperando.',
-  }
+    other: 'tenemos # clientes esperando.',
+  };
 
   deleteClient() {
     this.clients.shift();
@@ -39,9 +48,19 @@ export class UncommonPageComponent {
 
   // KeyValue Pipe
   public person = {
-    name: "Raúl",
+    name: 'Raúl',
     age: 31,
-    address: 'Sevilla, España'
-  }
+    address: 'Sevilla, España',
+  };
 
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(1000).pipe(
+    tap((value) => console.log('tap:', value))
+  );
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+    }, 3500);
+  });
 }
