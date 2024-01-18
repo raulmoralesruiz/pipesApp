d7585a8557aa8235e12e64c410619cfad8af330c
Raúl Morales <raulmdaw@gmail.com>
2024-01-18 20:03:10 +0100

    162. I18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Raúl';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Meli';
+    this.gender = 'female';
+  }
 
 }
