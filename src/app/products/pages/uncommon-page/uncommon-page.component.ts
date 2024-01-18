3ffa7113bd15aaa1dbe779af05a7aa3305a2531b
Raúl Morales <raulmdaw@gmail.com>
2024-01-18 20:27:05 +0100

    165. KeyValuePipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -37,4 +37,11 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
+  // KeyValue Pipe
+  public person = {
+    name: "Raúl",
+    age: 31,
+    address: 'Sevilla, España'
+  }
+
 }
