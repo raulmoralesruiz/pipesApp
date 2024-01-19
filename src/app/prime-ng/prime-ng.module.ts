039455aad5cdab1d38228351b8654d759e82f135
Raúl Morales <raulmdaw@gmail.com>
2024-01-19 14:53:29 +0100

    174. PrimeNG - Table

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,6 +6,7 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
 import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
@@ -16,6 +17,7 @@ import { ToolbarModule } from 'primeng/toolbar';
     MenubarModule,
     PanelModule,
     ToolbarModule,
+    TableModule,
   ]
 })
 export class PrimeNgModule { }
